c42646ad3cb2fcf63f05a86aad27d40994920b74
Lucas Pacheco Drozdowski <lucaspdroz@gmail.com>
2020-10-17 02:45:58 -0300

    Update router.js

--- src/router.js
+++ src/router.js
@@ -9,7 +9,9 @@ import InitialPage from './screens/InitialPage';
 export default () => (
     <Router>
         <div>
-            <Header></Header>
+            <Header>
+    <script id="mcjs">!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/b0afdd473b5ba7ba2c70c2458/dcdb103bcd3f5824cbf9edc97.js");</script>
+            </Header>
             <Route exact path='/' component={InitialPage} />
             <Route path='/edit/:id' component={Edit} />
             <Route path='/create' component={Create} />
